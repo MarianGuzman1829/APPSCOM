73941d1c0c7033ab51a694bcedcc6cb6418f855b
Atl Cardoso <atl1cardoso0@gmail.com>
2024-11-16 17:30:34 -0600

    Se agregaron nuevas constantes de colores

--- lib/src/res/colors.dart
+++ lib/src/res/colors.dart
@@ -1,5 +1,12 @@
 import 'package:flutter/material.dart';
 
+
+const Color backgroundColor2 = Color(0xFF17203A);
+const Color backgroundColorLight = Color(0xFFF2F6FF);
+const Color backgroundColorDark = Color(0xFF25254B);
+const Color shadowColorLight = Color(0xFF4A5367);
+const Color shadowColorDark = Colors.black;
+
 class AppColors {
   AppColors._();
 
