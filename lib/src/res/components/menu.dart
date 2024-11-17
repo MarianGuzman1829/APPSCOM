03fc95502f4c5d09d3d7d464f504f24dc07ac243
Atl Cardoso <atl1cardoso0@gmail.com>
2024-11-16 18:39:34 -0600

    Se creo un menu para poderlo utilizar en nuestro punto de entrada

---
+++ lib/src/res/components/menu.dart
@@ -0,0 +1,93 @@
+import 'rive_model.dart';
+
+class Menu {
+  final String title;
+  final RiveModel rive;
+
+  Menu({required this.title, required this.rive});
+}
+
+List<Menu> sidebarMenus = [
+  Menu(
+    title: "Home",
+    rive: RiveModel(
+        src: "assets/RiveAssets/icons.riv",
+        artboard: "HOME",
+        stateMachineName: "HOME_interactivity"),
+  ),
+  Menu(
+    title: "Busqueda",
+    rive: RiveModel(
+        src: "assets/RiveAssets/icons.riv",
+        artboard: "BUSQUEDA",
+        stateMachineName: "BUSQUEDA_Interactivity"),
+  ),
+  Menu(
+    title: "Favoritos",
+    rive: RiveModel(
+        src: "assets/RiveAssets/icons.riv",
+        artboard: "LIKE/STAR",
+        stateMachineName: "STAR_Interactivity"),
+  ),
+  Menu(
+    title: "Ayuda",
+    rive: RiveModel(
+        src: "assets/RiveAssets/icons.riv",
+        artboard: "CHAT",
+        stateMachineName: "CHAT_Interactivity"),
+  ),
+];
+List<Menu> sidebarMenus2 = [
+  Menu(
+    title: "Historial",
+    rive: RiveModel(
+        src: "assets/RiveAssets/icons.riv",
+        artboard: "TIEMPO",
+        stateMachineName: "TIEMPO_Interactivity"),
+  ),
+  Menu(
+    title: "Notifications",
+    rive: RiveModel(
+        src: "assets/RiveAssets/icons.riv",
+        artboard: "BELL",
+        stateMachineName: "BELL_Interactivity"),
+  ),
+];
+
+List<Menu> bottomNavItems = [
+  Menu(
+    title: "Chat",
+    rive: RiveModel(
+        src: "assets/RiveAssets/icons.riv",
+        artboard: "CHAT",
+        stateMachineName: "CHAT_Interactivity"),
+  ),
+  Menu(
+    title: "BUSQUEDA",
+    rive: RiveModel(
+        src: "assets/RiveAssets/icons.riv",
+        artboard: "BUSQUEDA",
+        stateMachineName: "BUSQUEDA_Interactivity"),
+  ),
+  Menu(
+    title: "Tiempo",
+    rive: RiveModel(
+        src: "assets/RiveAssets/icons.riv",
+        artboard: "TIEMPO",
+        stateMachineName: "TIEMPO_Interactivity"),
+  ),
+  Menu(
+    title: "Notificaciones",
+    rive: RiveModel(
+        src: "assets/RiveAssets/icons.riv",
+        artboard: "BELL",
+        stateMachineName: "BELL_Interactivity"),
+  ),
+  Menu(
+    title: "Perfil",
+    rive: RiveModel(
+        src: "assets/RiveAssets/icons.riv",
+        artboard: "USER",
+        stateMachineName: "USER_Interactivity"),
+  ),
+];
--- lib/src/screens/entrypoint/entry_point.dart
+++ lib/src/screens/entrypoint/entry_point.dart
@@ -2,3 +2,6 @@ import 'dart:math';
 
 import 'package:flutter/material.dart';
 import 'package:rive/rive.dart';
+import 'package:appscom/src/res/colors.dart';
+import 'package:appscom/src/screens/home/home_page.dart';
+import 'package:appscom/src/utils/rive_utils.dart';
